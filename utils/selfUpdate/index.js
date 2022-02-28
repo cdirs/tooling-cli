efd396d02092c6e4e24a0f581eec1dc9e38a5704
Simon Skinner <vultuk28@gmail.com>
2022-02-28 13:30:09 +0000

    fix(codestyle): added codestyle update on self update

--- utils/selfUpdate/index.js
+++ utils/selfUpdate/index.js
@@ -2,11 +2,14 @@ const run = require('../run');
 const ora = require('ora');
 const spinner = ora({ text: '' });
 const alert = require('cli-alerts');
+const codeStyle = require('../codeStyle');
 
 module.exports = async () => {
   spinner.start(`Installing latest version`);
   await run(`npm install t3k@latest --global`);
   spinner.succeed(`Latest version installed`);
+
+  await codeStyle();
 
   alert({
     type: `success`,
