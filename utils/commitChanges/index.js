ffa5d8d3f555e2b5c55a7518273d5941772c30f0
Simon Skinner <vultuk28@gmail.com>
2022-02-28 07:54:50 +0000

    feat(commit): added commit message generator
    Write a commit message generator imitating the current commitizen one for easy commit messages
    
    Refs: fixes #1

--- utils/commitChanges/index.js
+++ utils/commitChanges/index.js
@@ -2,10 +2,42 @@ const run = require('../run');
 const questions = require('./questions');
 
 module.exports = async (addAll = false, autoPush = false) => {
-	if (addAll) {
-		// Always pull first
-		await run('git pull');
+	// Always pull first
+	await run('git pull');
 
+	const {
+		addAll: addAllAnswer,
+		autoPush: autoPushAnswer,
+		type,
+		scope,
+		description,
+		longDescription,
+		breaking,
+		breakingDescription,
+		fixes,
+		issueReferences
+	} = await questions(addAll, autoPush);
+
+	const commitMessage = `${type}${scope.length > 0 ? `(${scope})` : ''}${
+		breaking ? '!' : ''
+	}: ${description}${
+		longDescription.length > 0
+			? `
+	
+${longDescription.length > 0 ? longDescription : ''}`
+			: ''
+	}${
+		breaking || fixes
+			? `
+
+`
+			: ''
+	}${breaking ? `BREAKING CHANGE: ${breakingDescription}` : ''}
+${fixes ? `Refs: ${issueReferences}` : ''}`;
+
+	console.log(commitMessage);
+
+	if (addAll == true || addAllAnswer === true) {
 		try {
 			await run('git add -A');
 		} catch (e) {
@@ -13,9 +45,13 @@ module.exports = async (addAll = false, autoPush = false) => {
 		}
 	}
 
-	const commitAnswers = questions(addAll, autoPush);
+	try {
+		await run(`git commit -m '${commitMessage}'`);
+	} catch (e) {
+		console.error(e);
+	}
 
-	if (autoPush) {
+	if (autoPush === true || autoPushAnswer === true) {
 		try {
 			await run('git push');
 		} catch (e) {
--- utils/commitChanges/questions.js
+++ utils/commitChanges/questions.js
@@ -23,44 +23,175 @@ module.exports = async (addAll = false, autoPush = false) => {
 		};
 	}
 
+	if (autoPush !== true) {
+		vars = {
+			...vars,
+			...(await ask([
+				{
+					type: 'toggle',
+					name: 'autoPush',
+					message: 'Push changes?',
+					hint: `Automatically push changes when commit is complete.`,
+					enabled: 'Yes',
+					disabled: 'No',
+					initial: autoPush
+				}
+			]))
+		};
+	}
+
+	vars = {
+		...vars,
+		...(await ask([
+			{
+				type: 'select',
+				name: 'type',
+				message: 'What time of change are you committing?',
+				initial: 1,
+				required: true,
+				choices: [
+					{
+						message: 'feat:',
+						value: 'feat',
+						hint: 'A new feature'
+					},
+					{
+						message: 'fix:',
+						value: 'fix',
+						hint: 'A bug fix'
+					},
+					{
+						message: 'docs:',
+						value: 'docs',
+						hint: 'Documentation only changes'
+					},
+					{
+						message: 'style:',
+						value: 'style',
+						hint: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
+					},
+					{
+						message: 'refactor:',
+						value: 'refactor',
+						hint: 'A code change that neither fixes a bug nor adds a feature'
+					},
+					{
+						message: 'perf:',
+						value: 'perf',
+						hint: 'A code change that improves performance'
+					},
+					{
+						message: 'test:',
+						value: 'test',
+						hint: 'Adding missing tests of correcting existing tests'
+					},
+					{
+						message: 'build:',
+						value: 'build',
+						hint: 'Changes that affect the build system or external dependencies (example scopes: gulp, npm)'
+					},
+					{
+						message: 'ci:',
+						value: 'ci',
+						hint: 'Changes to out CI/CD configuration files and scripts'
+					},
+					{
+						message: 'chore:',
+						value: 'chore',
+						hint: "Other changes that don't modify src or test files"
+					},
+					{
+						message: 'revert:',
+						value: 'revert',
+						hint: 'Reverts a previous commit'
+					}
+				]
+			},
+			{
+				type: 'input',
+				name: 'scope',
+				message: 'What is the scope of this change (Optional):',
+				hint: `(e.g. component or file name)`,
+				required: false,
+				format: val => val.toLowerCase()
+			}
+		]))
+	};
+
+	const remainingCharacters = 92 - vars.scope.length;
+
+	vars = {
+		...vars,
+		...(await ask([
+			{
+				type: 'input',
+				name: 'description',
+				message: `Write a short, imperative tense description (max ${remainingCharacters} chars):`,
+				required: true,
+				validate: val =>
+					val.length <= remainingCharacters
+						? true
+						: `Too long, maxiumum is ${remainingCharacters} characters and you used ${val.length}.`
+			},
+			{
+				type: 'input',
+				name: 'longDescription',
+				message:
+					'Provide a longer description of the change (Optional):',
+				required: false
+			},
+			{
+				type: 'toggle',
+				name: 'breaking',
+				message: 'Are there any breaking changes?',
+				enabled: 'Yes',
+				disabled: 'No',
+				initial: false
+			}
+		]))
+	};
+
+	if (vars.breaking === true) {
+		vars = {
+			...vars,
+			...(await ask([
+				{
+					type: 'input',
+					name: 'breakingDescription',
+					message: 'Describe the breaking changes:',
+					required: true
+				}
+			]))
+		};
+	}
+
 	vars = {
 		...vars,
 		...(await ask([
 			{
 				type: 'toggle',
-				name: 'delete',
-				message: 'Delete current automations?',
-				hint: `Remove all traces of previous automations ready for updated ones.`,
+				name: 'fixes',
+				message: 'Does this change affect any open issues?',
 				enabled: 'Yes',
 				disabled: 'No',
-				initial: true
-			},
-			{
-				type: 'select',
-				name: 'type',
-				message: 'Project Type?',
-				initial: 1,
-				required: true,
-				choices: [
-					{
-						message: 'Netlify',
-						value: 'netlify',
-						hint: 'For a website that will be deployed to Netlify'
-					},
-					{
-						message: 'Microservice',
-						value: 'microservice',
-						hint: 'For a Microservice that will be deployed to Google Cloud'
-					},
-					{
-						message: 'NPM Package',
-						value: 'npmPackage',
-						hint: 'For a standard NPM package where no extra build steps are required'
-					}
-				]
+				initial: false
 			}
 		]))
 	};
+
+	if (vars.fixes === true) {
+		vars = {
+			...vars,
+			...(await ask([
+				{
+					type: 'input',
+					name: 'issueReferences',
+					message: 'Add issue references (e.g. "fixes #123"):',
+					required: true
+				}
+			]))
+		};
+	}
 
 	return vars;
 };
