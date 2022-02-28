c4b4a38b42ce29d57328324eef6f984187fff565
Simon Skinner <vultuk28@gmail.com>
2022-02-28 08:41:38 +0000

    ci(update): updated actions and other files to the latest version

--- utils/updateProject/index.js
+++ utils/updateProject/index.js
@@ -14,92 +14,86 @@ const commitAndRecoverStash = require('./commitAndRecoverStash');
 const spinner = ora({ text: '' });
 
 module.exports = async () => {
-	const vars = await questions();
+  const vars = await questions();
 
-	spinner.start(`Stashing current changes`);
-	await run('git stash');
-	spinner.succeed(`Current changes stashed`);
+  spinner.start(`Stashing current changes`);
+  await run('git stash');
+  spinner.succeed(`Current changes stashed`);
 
-	spinner.start(`Generating Automations`);
+  spinner.start(`Generating Automations`);
 
-	if (vars.delete === true) await del('.github');
+  if (vars.delete === true) await del('.github');
 
-	// Make the required directories
-	await new Promise((resolve, reject) => {
-		fs.mkdir('.github/workflows', { recursive: true }, err => {
-			if (err) reject(err);
+  // Make the required directories
+  await new Promise((resolve, reject) => {
+    fs.mkdir('.github/workflows', { recursive: true }, err => {
+      if (err) reject(err);
 
-			resolve();
-		});
-	});
+      resolve();
+    });
+  });
 
-	const actionYaml = yaml.load(
-		await axios(
-			`https://raw.githubusercontent.com/tech3k/actions/main/templates/${vars.type}.yml`
-		).then(r => r.data)
-	);
+  const actionYaml = yaml.load(
+    await axios(
+      `https://raw.githubusercontent.com/tech3k/actions/main/templates/${vars.type}.yml`
+    ).then(r => r.data)
+  );
 
-	// Add in service name
-	if (vars.serviceName) {
-		actionYaml.jobs.CloudRun['with'] = { SERVICE_NAME: vars.serviceName };
-	}
-	// Add in base url
-	if (vars.baseUrl) {
-		actionYaml.jobs.Netlify['with'] = { NETLIFY_URL: vars.baseUrl };
-	}
+  // Add in service name
+  if (vars.serviceName) {
+    actionYaml.jobs.CloudRun['with'] = { SERVICE_NAME: vars.serviceName };
+  }
+  // Add in base url
+  if (vars.baseUrl) {
+    actionYaml.jobs.Netlify['with'] = { NETLIFY_URL: vars.baseUrl };
+  }
 
-	fs.writeFileSync(
-		'.github/workflows/deploy.yml',
-		yaml.dump({ ...actionYaml })
-	);
+  fs.writeFileSync(
+    '.github/workflows/deploy.yml',
+    yaml.dump({ ...actionYaml })
+  );
 
-	spinner.succeed(`Automations Generated`);
+  spinner.succeed(`Automations Generated`);
 
-	if (vars.gitpod === true) {
-		spinner.start(`Setting up GitPod`);
-		let gitpodYaml = {};
-		if (fs.existsSync('.gitpod.yml')) {
-			gitpodYaml = yaml.load(fs.readFileSync('.gitpod.yml', 'utf8'));
-		} else {
-			gitpodYaml = { ...(gitpodYamlDefaults[vars.type] ?? {}) };
-		}
+  if (vars.gitpod === true) {
+    spinner.start(`Setting up GitPod`);
+    let gitpodYaml = {};
+    if (fs.existsSync('.gitpod.yml')) {
+      gitpodYaml = yaml.load(fs.readFileSync('.gitpod.yml', 'utf8'));
+    } else {
+      gitpodYaml = { ...(gitpodYamlDefaults[vars.type] ?? {}) };
+    }
 
-		const gitpodDump = { ...gitpodYaml, ...defaultGitPodYaml };
+    const gitpodDump = { ...gitpodYaml, ...defaultGitPodYaml };
 
-		console.log(gitpodDump.tasks);
+    console.log(gitpodDump.tasks);
 
-		if (!gitpodDump.tasks) {
-			gitpodDump.tasks = {
-				before: 'npm install t3k --global'
-			};
-		} else if (!gitpodDump.tasks?.before) {
-			gitpodDump.tasks['before'] = 'npm install t3k --global';
-		}
-		// 		fs.writeFileSync(
-		// 			'.gitpod.Dockerfile',
-		// 			`FROM gitpod/workspace-full
+    if (!gitpodDump.tasks) {
+      gitpodDump.tasks = {
+        before: 'npm install t3k --global'
+      };
+    } else if (!gitpodDump.tasks.before) {
+      gitpodDump.tasks.before = 'npm install t3k --global';
+    }
 
-		// RUN npm install t3k tk-cz --global`
-		// 		);
+    fs.writeFileSync('.gitpod.yml', yaml.dump(gitpodDump, { indent: 2 }));
 
-		fs.writeFileSync('.gitpod.yml', yaml.dump(gitpodDump));
+    spinner.succeed(`GitPod Ready`);
+  }
 
-		spinner.succeed(`GitPod Ready`);
-	}
+  if (vars.codeStandards === true) {
+    spinner.start(`Synching Code Standards`);
+    // Code Standards
+    spinner.succeed(`Code Standards Synched`);
+  }
 
-	if (vars.codeStandards === true) {
-		spinner.start(`Synching Code Standards`);
-		// Code Standards
-		spinner.succeed(`Code Standards Synched`);
-	}
+  await commitAndRecoverStash(
+    `ci(update): updated actions and other files to the latest version`
+  );
 
-	await commitAndRecoverStash(
-		`ci(update): updated actions and other files to the latest version`
-	);
-
-	alert({
-		type: `success`,
-		name: `ALL DONE`,
-		msg: `Project is ready to go`
-	});
+  alert({
+    type: `success`,
+    name: `ALL DONE`,
+    msg: `Project is ready to go`
+  });
 };
